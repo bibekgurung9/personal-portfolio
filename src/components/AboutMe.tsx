9e8b246b6de660c27fd2048bd9765a28d533f3ea
Bibek Gurung <100699318+bibekgurung9@users.noreply.github.com>
2024-01-07 19:01:08 +0545

    addded about me image

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,4 +1,5 @@
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import about from '@/assets/about.png'
+import Image from 'next/image'
 
 export default function Example() {
   return (
@@ -16,9 +17,9 @@ export default function Example() {
           </div>
         </div>
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-          <img
-            className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+          <Image
+            className="w-[52rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+            src={about}
             alt=""
           />
         </div>
