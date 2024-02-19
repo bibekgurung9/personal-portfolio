dc5a8b407edd4445e2cacda80568aaf7dda4fd21
Bibek Gurung <100699318+bibekgurung9@users.noreply.github.com>
2024-02-19 21:48:15 +0545

    finish portfolio section

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { posts } from '@/constants'
 import Image from 'next/image'
+import { FaGithub, FaLink, FaClock } from 'react-icons/fa6'
 
 const Portfolio = () => {
   return (
@@ -11,18 +12,24 @@ const Portfolio = () => {
             Check My Projects & Works
           </p>
           
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none md:grid-cols-2 lg:grid-cols-3">
           {posts.map((post) => (
-            <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
-              <Image src={''} alt={''} />  
-              <div className="group relative">
-                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                  <a href={post.href}>
-                    <span className="absolute inset-0" />
-                    {post.title}
-                  </a>
+            <article key={post.id} className="flex max-w-xl flex-col items-center justify-center ">
+              <div>
+                <Image alt={''} src={post.src} width={300} height={300}></Image>  
+              </div>
+              <div className="group relative cursor-default">
+                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600 cursor-default">
+                  {post.title}
                 </h3>
                 <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
+
+                <div className='flex gap-x-4 justify-center items-center mt-2'>
+                  <p className='flex justify-center items-center gap-x-2 font-bold bg-slate-300/60 p-2 rounded-full border border-slate-400 hover:shadow-2xl'><FaClock />{post.status}</p>
+                  <a href={post.github} className='flex justify-center items-center gap-x-2 font-bold bg-slate-300/60 p-2 rounded-full border border-slate-400 hover:shadow-2xl' target='blank'><FaGithub />Github</a>
+                  <a href={post.href} className='flex justify-center items-center gap-x-2 font-bold bg-slate-300/60 p-2 rounded-full border border-slate-400 hover:shadow-2xl' target='blank'><FaLink />Link</a>
+                </div>
+
               </div>
             </article>
           ))}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,12 +7,12 @@ export const navigation = [
 ]
 
 //images for tech stack
-import html from '@/assets/html.svg'
-import css from '@/assets/css.svg'
-import javascript from '@/assets/javascript.svg'
-import react from '@/assets/react.svg'
-import tailwind from '@/assets/tailwind.svg'
-import nextjs from '@/assets/nextjs.svg'
+import html from '@/assets/stack/html.svg'
+import css from '@/assets/stack/css.svg'
+import javascript from '@/assets/stack/javascript.svg'
+import react from '@/assets/stack/react.svg'
+import tailwind from '@/assets/stack/tailwind.svg'
+import nextjs from '@/assets/stack/nextjs.svg'
 
 export const techStack = [
     {src: html, alt: 'HTML'},
@@ -22,52 +22,62 @@ export const techStack = [
     {src: tailwind, alt: 'TAILWIND'},
     {src: nextjs, alt: 'NEXTJS'},
 ]
+
+import project1 from '@/assets/projects/project 1.png'
+import project2 from '@/assets/projects/project 2.png'
+import project3 from '@/assets/projects/project 3.png'
+import project4 from '@/assets/projects/project 4.png'
+import noImage from '@/assets/projects/no image.jpg'
+
 //portfolio section
 export const posts = [
     {
       id: 1,
       title: 'Freelancing Website With NextJs 14',
-      href: '#',
-      github: 'https://github.com/bibekgurung9/php-ecommerce-app',
-      name: 'NextJS Ecommerce',
+      src: noImage,
+      href: 'https://github.com/bibekgurung9/nextjs-freelance-website',
+      github: 'https://github.com/bibekgurung9/nextjs-freelance-website',
+      name: 'NextJS Freelance',
       status: 'Ongoing',
       description:
-        'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',   
+        'This project is a freelance application built with Nextjs14, designed to provide a platform for freelancers to showcase their skills and services. Clients can explore and hire freelancers for various projects.',   
     },
     {
       id: 2,
       title: 'Realtime ChatRoom with React & Appwrite',
+      src: project2,
       href: 'https://glittering-parfait-71946c.netlify.app/',
       github: 'https://github.com/bibekgurung9/php-ecommerce-app',
       name: 'Realtime ChatRoom App',
       status: 'Ongoing',
       description:
-        'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',   
+        'This project is a real-time chatroom application built using React, Vite, and Appwrite. Users can seamlessly sign up, log in, and exchange messages in a chatroom in real-time. The integration with Appwrite provides a secure and scalable backend for user management and message storage, while Vite ensures a fast and efficient development environment.',   
     },
     {
       id: 3,
       title: 'Fitness Website with NextJs14 & Rapid Api',
+      src: project3,
       href: 'https://fitness-exercise-app-eight.vercel.app/',
-      github: 'https://github.com/bibekgurung9/php-ecommerce-app',
+      github: 'https://github.com/bibekgurung9/fitness-exercise-app',
       name: 'Fitness Wesbite',
       status: 'Completed',
       description:
-        'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',   
+        'This project is a front-end fitness app built with Next.js 14, TypeScript, Tailwind CSS, and ShadcnUI. This application leverages the power of Rapid API to provide you with comprehensive fitness data, helping you achieve your health and wellness goals.',   
     },
     {
         id: 4,
         title: 'Ecommerce Website With PHP & MySQL',
+        src: project4,
         href: 'https://github.com/bibekgurung9/php-ecommerce-app',
         github: 'https://github.com/bibekgurung9/php-ecommerce-app',
         name: 'PHP Ecommerce',
         status: 'Completed',
         description:
-          'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',   
+          'This project is a full-stack e-commerce website project built with HTML, CSS, JavaScript, PHP, and MySQL. It provides a platform for users to browse, purchase, and manage projects with admin dashboard to manage users, products, orders, payment & more!',   
       },
   ]
 
   //contact page
-  import email from '@/assets/email.svg'
   import github from '@/assets/github.svg'
   import linkedin from '@/assets/linkedin1.svg'
 
