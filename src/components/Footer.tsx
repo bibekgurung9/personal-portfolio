d25e17420db7d79c1dc34c9e3ff3824be82be9fc
Bibek Gurung <100699318+bibekgurung9@users.noreply.github.com>
2024-02-19 22:06:06 +0545

    fixed footer and nav

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,78 +1,66 @@
-import Image from 'next/image'
-import { footerLinks, socialLinks, posts, navigation } from '@/constants'
+import React from 'react'
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa6'
 
 export default function Example() {
-  //send email function
-  const sendEmail = ()  => {
-    console.log('sent email');
-    
-  }
-
   return (
-    <div className="justify-center bg-slate-200 py-4 pt-16 sm:pt-8 sm:py-4 px-8">
-      <div className='flex flex-row justify-between'>
-        <div id='col-1'>  
-          <a href="#" className='font-bold text-2xl'>Bibek Gurung</a>
-          <div className='flex flex-row gap-3'>
-          {footerLinks.map((footer, index) => (
-          <div className="mt-2 items-center justify-center">
-            <a href={footer.links} key={index}><Image src={footer.src} alt={''} width={35}/></a>
+      <footer className="bg-white">
+          <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+              <div className="md:flex md:justify-between">
+                <div className="mb-6 md:mb-0">
+                  <span className="self-center text-2xl font-semibold whitespace-nowrap">Bibek Gurung</span>
+                </div>
+                <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                    <div>
+                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Projects</h2>
+                        <ul className="text-gray-500 font-medium">
+                        <li className="mb-4">
+                                <a href="https://github.com/bibekgurung9/nextjs-freelance-website" className="hover:underline" target='blank'>Freelancer App</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="https://glittering-parfait-71946c.netlify.app/" className="hover:underline" target='blank'>Realtime Chatroom</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="https://fitness-exercise-app-eight.vercel.app/" className="hover:underline" target='blank'>PHP Ecommerce</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Contact me</h2>
+                        <ul className="text-gray-500 font-medium">
+                            <li className="mb-4">
+                                <a href="https://github.com/bibekgurung9" className="hover:underline ">Github</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="https://www.linkedin.com/in/bibekgurung9/" className="hover:underline">Linkedin</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="https://twitter.com/_BibekGurung" className="hover:underline">Twitter</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">CV</h2>
+                        <ul className="text-gray-500 font-medium">
+                            <li>
+                                <a href="https://docs.google.com/document/d/1uXvjcpXFdvfI7gj74FqEjNEHSslshgeuLofac7Bmr2c/edit?usp=sharing" className="hover:underline" target='blank'>Download Here</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-300 lg:my-8" />
+            <div className="sm:flex sm:items-center sm:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center">© 2024 <a href="https://flowbite.com/" className="hover:underline">Bibek Gurung</a> . All Rights Reserved.
+                </span>
+                <div className="flex mt-4 sm:justify-center sm:mt-0 gap-x-4">
+                  <a href="https://github.com/bibekgurung9" target='blank' className='text-black hover:text-black/70'><FaGithub/></a>
+                  <a href="https://www.linkedin.com/in/bibekgurung9/" target='blank' className='text-black hover:text-black/70'><FaLinkedin/></a>
+                  <a href="https://twitter.com/_BibekGurung" target='blank' className='text-black hover:text-black/70'><FaTwitter /></a>
+                </div>
+            </div>
           </div>
-        ))}
-        </div>
-        </div>
-
-        <div id='col-2'>
-          <ul className=''>
-          <li className='font-bold'>Quick Links</li>
-          {navigation.map((nav, index) => {
-              return (
-                <li key={index} className="flex p-1 justify-between mt-1 hover:bg-purple-200 hover:rounded">
-                  <a className="flex gap-2" href={nav.href} target='blank'>
-                      <p className="">{nav.name}</p>
-                  </a>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-
-        <div id='col-3'>
-        <ul className=''>
-            <li className='font-bold'>Other Projects</li>
-            {posts.map((projects) => {
-              return (
-                <li key={projects.id} className="flex p-1 justify-between mt-1 hover:bg-purple-200 hover:rounded">
-                  <a className="flex gap-2" href={projects.href} target='blank'>
-                      <p className="truncate">{projects.name}</p>
-                  </a>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-
-        <div id='col-4'>
-        <ul className=''>
-            <li className='font-bold'>Contact Me</li>
-            {socialLinks.map((socials) => {
-              return (
-                <li key={socials.id} className="flex p-1 justify-between mt-1 hover:bg-purple-200 hover:rounded">
-                  <a className="flex gap-2" href={socials.links} target='blank'>
-                    <Image className="flex-nonebg-gray-50" alt="github" width={25} src={socials.src} />
-                      <p className="truncate">{socials.name}</p>
-                  </a>
-                </li>
-              )
-            })}
-            <button className='p-2 bg-purple-300 mt-2 rounded-full' onClick={sendEmail}>Send Email</button>
-          </ul>
-        </div>
-      </div>
-      <div className='mt-10 flex justify-center'>
-      <span className='text-xl text-center font-semibold'>This Portfolio Website Was Made My<a href="www.github.com/bibekgurung9" className='cursor-pointer hover:text-red-500' target='blank'> Bibek Gurung</a></span>
-      </div>
-    </div>
-    
+      </footer> 
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { navigation } from '@/constants'
-import Image from 'next/image'
-import download from '@/assets/download.svg'
 
 const NavBar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
